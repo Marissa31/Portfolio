6441dfcf43a9bd8510d72babca52dce0170b824d
Marissa <marissa.aramouni.1@ul.edu.lb>
2025-09-29 13:28:10 +0300

    Initial portfolio commit

---
+++ marissa-portfolio/src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css"; // Tailwind styles
+import MarissaPortfolio from "./MarissaPortfolio";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <MarissaPortfolio />
+  </React.StrictMode>
+);
